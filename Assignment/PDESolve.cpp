b21ee593af3a493d3672522fa6a3824e21ee8034
Augustin <a.reille@net.estia.fr>
2017-10-18 18:16:27 +0100

    removing pdf 2

--- Assignment/PDESolve.cpp
+++ Assignment/PDESolve.cpp
@@ -17,7 +17,6 @@ PDESolve::PDESolve(double D, double dx, double dt, double L, double T, double Te
     this->nspace = L / dx;
     this->ntime = T / dt;
     this->results = Matrix(this->ntime, this->nspace);
-    
 
     // Initialisation with boudary conditions
     for (int i = 0; i < nspace; i++)
